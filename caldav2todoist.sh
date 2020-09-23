4bbcda47dde6e44699c346dc59e82c413419831d
Christian Baer <chris@debilux.org>
2020-09-23 07:57:23 +0200

    Convert tabs to spaces.

--- caldav2todoist.sh
+++ caldav2todoist.sh
@@ -50,23 +50,23 @@ caldav_timezone="UTC"
 ## Check if script is already running
 ## Usage: checkpid
 checkPID () {
-	SELF_pid=$$
-	SELF_pid_file="${BASEDIR}/.${SELF}.pid"
+  SELF_pid=$$
+  SELF_pid_file="${BASEDIR}/.${SELF}.pid"
 
-	if [ -z ${SELF_pid_file} ]; then
-		# Get stored PID from file
-		SELF_stored_pid=`cat ${SELF_pid_file}`
+  if [ -z ${SELF_pid_file} ]; then
+    # Get stored PID from file
+    SELF_stored_pid=`cat ${SELF_pid_file}`
 
-		# Check if stored PID is in use
-		SELF_pid_is_running=`ps aux | awk '{print $2}' | grep ${SELF_stored_pid}`
+    # Check if stored PID is in use
+    SELF_pid_is_running=`ps aux | awk '{print $2}' | grep ${SELF_stored_pid}`
 
-		if [ "${SELF_pid_is_running}" ]; then
-			# If stored PID is already in use, skip execution
-			exit 1
-		fi
-	fi
-	# Update PID file
-	echo ${SELF_pid} > ${SELF_pid_file}
+    if [ "${SELF_pid_is_running}" ]; then
+      # If stored PID is already in use, skip execution
+      exit 1
+    fi
+  fi
+  # Update PID file
+  echo ${SELF_pid} > ${SELF_pid_file}
 }
 
 ## Get the items from the given calDAV URL that are tasks, not completed and
@@ -74,27 +74,27 @@ checkPID () {
 ## Pipe the through grep and cut to get only the task contents in an array
 ## Usage: caldav_get_task_list
 caldav_get_task_list() {
-	curl --silent \
-		--request REPORT \
-		--header "Depth: 1" \
-		--header "Content-Type: text/xml" \
-		--data "<c:calendar-query xmlns:d='DAV:' xmlns:c='urn:ietf:params:xml:ns:caldav'>
-		<d:prop><d:getetag /><c:calendar-data /></d:prop>
-		<c:filter>
-		<c:comp-filter name='VCALENDAR'>
-		<c:comp-filter name='VTODO'>
-		${LASTRUN}
-		<c:prop-filter name='COMPLETED'>
-		<c:is-not-defined/>
-		</c:prop-filter>
-		</c:comp-filter>
-		</c:comp-filter>
-		</c:filter>
-		</c:calendar-query>" \
-		--user "${caldav_login}:${caldav_password}" \
-		${caldav_url} | \
-		grep "SUMMARY" | \
-		cut -f 2 -d : >> ${tasks_file}
+  curl --silent \
+    --request REPORT \
+    --header "Depth: 1" \
+    --header "Content-Type: text/xml" \
+    --data "<c:calendar-query xmlns:d='DAV:' xmlns:c='urn:ietf:params:xml:ns:caldav'>
+    <d:prop><d:getetag /><c:calendar-data /></d:prop>
+    <c:filter>
+    <c:comp-filter name='VCALENDAR'>
+    <c:comp-filter name='VTODO'>
+    ${LASTRUN}
+    <c:prop-filter name='COMPLETED'>
+    <c:is-not-defined/>
+    </c:prop-filter>
+    </c:comp-filter>
+    </c:comp-filter>
+    </c:filter>
+    </c:calendar-query>" \
+    --user "${caldav_login}:${caldav_password}" \
+    ${caldav_url} | \
+    grep "SUMMARY" | \
+    cut -f 2 -d : >> ${tasks_file}
 }
 
 ## Create a UUID and a Temp-ID for the task.
@@ -102,20 +102,20 @@ caldav_get_task_list() {
 ## Suppress output except there is an error.
 ## Usage: todoist_add
 todoist_add() {
-	## Create a UUID and a Temp-ID for the task
-	task_uuid=`uuidgen`
-	task_tempid=`uuidgen`
+  ## Create a UUID and a Temp-ID for the task
+  task_uuid=`uuidgen`
+  task_tempid=`uuidgen`
 
-	task_query="{ \"type\": \"item_add\", \
-								\"temp_id\": \"${task_tempid}\", \
-								\"uuid\": \"${task_uuid}\", \
-								\"args\": { \"content\": \"${task}\" } \
-							}"
+  task_query="{ \"type\": \"item_add\", \
+                \"temp_id\": \"${task_tempid}\", \
+                \"uuid\": \"${task_uuid}\", \
+                \"args\": { \"content\": \"${task}\" } \
+              }"
 
-	curl --silent --show-error --output /dev/null --fail \
-		${todoist_api_endpoint} \
-		-d token="${todoist_api_token}" \
-		-d commands="[${task_query}]"
+  curl --silent --show-error --output /dev/null --fail \
+    ${todoist_api_endpoint} \
+    -d token="${todoist_api_token}" \
+    -d commands="[${task_query}]"
 }
 
 ## Let's start the hustle
@@ -123,10 +123,10 @@ checkPID
 
 ## Check if we've been running before and get the timestamp of the last run
 if [ -f ${lastrun_file} ]; then
-	TIMESTAMP_LAST=`cat ${lastrun_file}`
-	LASTRUN="<c:prop-filter name='CREATED'>
-	<c:time-range start='${TIMESTAMP_LAST}'/>
-	</c:prop-filter>"
+  TIMESTAMP_LAST=`cat ${lastrun_file}`
+  LASTRUN="<c:prop-filter name='CREATED'>
+  <c:time-range start='${TIMESTAMP_LAST}'/>
+  </c:prop-filter>"
 fi
 
 ## Build the current timestamp (mind the T and Z chars!)
@@ -137,13 +137,13 @@ caldav_get_task_list
 ## Continue if the list is not empty
 ## Push every task to todoist
 if [ -s "${tasks_file}" ]; then
-	touch ${tasks_file}.tmp
-	while read task
-	do
-		## Create task at todoist and keep the task in buffer if it fails
+  touch ${tasks_file}.tmp
+  while read task
+  do
+    ## Create task at todoist and keep the task in buffer if it fails
     todoist_add || echo ${task} >> ${tasks_file}.tmp
-	done < ${tasks_file}
-	mv ${tasks_file}.tmp ${tasks_file}
+  done < ${tasks_file}
+  mv ${tasks_file}.tmp ${tasks_file}
 fi
 
 ## Update the timestamp of the last run
